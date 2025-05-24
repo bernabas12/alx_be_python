28d3f853de49e8c9d10fbf5f3ee101b2953a33df
bernabas12 <bernabas123@gmail.com>
2025-05-24 00:41:54 -0700

    first pythone code

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,12 +1,12 @@
 #To practice basic arithmetic operations in Python by performing predefined calculations
-number_1 = 10
-number_2 = 5
+number1 = 10
+number2 = 5
 # Addition
-sum = number_1 + number_2
-print(f"Addition of {number_1} and {number_2} is: {sum}")
+sum = number1 + number2
+print(f"Addition of {number1} and {number2} is: {sum}")
 # Subtraction
-difference = number_1 - number_2
-print(f"Subtraction of {number_1} and {number_2} is: {difference}")
+difference = number1 - number2
+print(f"Subtraction of {number1} and {number2} is: {difference}")
 # Multiplication
-product = number_1 * number_2
-print(f"Multiplication of {number_1} and {number_2} is: {product}")
+product = number1 * number2
+print(f"Multiplication of {number1} and {number2} is: {product}")
