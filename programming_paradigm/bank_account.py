e79c9abbde6c05ea31eed29dad679ee1dd84b1f6
bernabas12 <bernabas123@gmail.com>
2025-06-13 00:39:46 -0700

    class1

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,8 +5,10 @@ class BankAccount:
         self.account_balance += amount
         return amount
     def withdraw(self, amount):
-        self.account_balance -= amount
-        return amount
-       
+        if amount > self.account_balance:
+            return None
+        else:   
+            self.account_balance -= amount
+            return amount
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
