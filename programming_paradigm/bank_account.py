143c06f2d0cc679160b2bfd8e19202d874f2572a
bernabas12 <bernabas123@gmail.com>
2025-06-12 23:53:19 -0700

    class

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self,account_balance = 0):
+        self.account_balance = account_balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+        else:
+            print("Deposit amount must be positive.")
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.account_balance:
+            self.account_balance -= amount
+        else:
+            print("Withdrawal amount must be positive and less than or equal to the account balance.")
+    def display_balance(self):
+        print(f"Current account balance: ${self.account_balance:.2f}")
