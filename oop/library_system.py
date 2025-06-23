afc1c127e568f011395b6c3e84ce58be175b2d67
bernabas12 <bernabas123@gmail.com>
2025-06-23 02:02:08 -0700

    oop

---
+++ oop/library_system.py
@@ -0,0 +1,29 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+class printBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book: Book):
+        self.books.append(book)
+
+    def list_books(self):
+        if not self.books:
+            print("The library is empty.")
+        else:
+            for book in self.books:
+                print(book.get_info())
+
